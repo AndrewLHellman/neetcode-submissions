45e48eb8bdd508946d2e04f751162c4faa72940d
Andrew Hellman <142917243+AndrewLHellman@users.noreply.github.com>
2026-08-10 23:31:13 -0500

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,36 @@
+from typing import DefaultDict
+
+class TrieNode:
+    children: DefaultDict[str, Optional[TrieNode]]
+    def __init__(self):
+        self.children = defaultdict(lambda: None)
+        self.word = False
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if curr.children[c] == None:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            curr = curr.children[c]
+            if curr == None:
+                return False
+        return curr.word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            curr = curr.children[c]
+            if curr == None:
+                return False
+        return True
+        
